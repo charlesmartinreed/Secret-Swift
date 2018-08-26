fe0c843aa13def91424b39c29afa3ff24b0615d6
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-08-26 15:09:00 -0500

    Working. Need fix for Persistent UIButton.

--- Project28/ViewController.swift
+++ Project28/ViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2018 Charles Martin Reed. All rights reserved.
 //
 
+import LocalAuthentication
 import UIKit
 
 class ViewController: UIViewController {
@@ -14,10 +15,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "Nothing to see here"
+        
         //MARK:- adding Observers in Notification center for adjusting layout during keyboard resizing
         let notificationCenter = NotificationCenter.default
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: Notification.Name.UIKeyboardWillHide, object: nil)
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: Notification.Name.UIKeyboardWillChangeFrame, object: nil)
+        //let us know when the user has left the app or entered multitask with UIApplicationWillResignActive
+        notificationCenter.addObserver(self, selector: #selector(saveSecretMessage), name: Notification.Name.UIApplicationWillResignActive, object: nil)
     }
     
     //MARK:- adjust keyboard
@@ -39,8 +44,63 @@ class ViewController: UIViewController {
         let selectedRange = secret.selectedRange
         secret.scrollRangeToVisible(selectedRange)
     }
+    
+    //MARK:- Authentication methods
+    func unlockSecretMessage() {
+        //show the text view
+        secret.isHidden = false
+        title = "Secret stuff!"
+        
+        //load the keychain's text
+        //result is optional, so unwrap it once you know there's a value
+        if let text = KeychainWrapper.standard.string(forKey: "SecretMessage") {
+            secret.text = text
+        }
+    }
+    
+    @objc func saveSecretMessage() {
+        //write the secret textView's contents to the keychain and then hide the view
+        //uses the set method of keychain wrapper
+        
+        if !secret.isHidden {
+            _ = KeychainWrapper.standard.set(secret.text, forKey: "SecretMessage")
+            secret.resignFirstResponder()
+            secret.isHidden = true
+            title = "Nothing to see here"
+        }
+    }
+    
 
     @IBAction func authenticateTapped(_ sender: Any) {
+        //we need to check if the device can use biometric authentication and then
+        //use it accordingly if they can, using LAContext class
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            //reason we give for why we want TouchID to be used... FaceID handled in info.plist
+            let reason = "Identify yourself, please."
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { [unowned self] (success, authenticationError) in
+                
+                //because this may not occur on the main thread and we need to use
+                //the main thread for UI updating, we'll use GCD
+                DispatchQueue.main.async {
+                    if success {
+                        self.unlockSecretMessage()
+                    } else {
+                        //give the error
+                        let ac = UIAlertController(title: "Authentication failed", message: "Your identity could not be verified. Please try again.", preferredStyle: .alert)
+                        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                        self.present(ac, animated: true, completion: nil)
+                    }
+                }
+            }
+        } else {
+            let ac = UIAlertController(title: "Biometry unavailable", message: "Your device is not configured for TouchID or FaceID.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(ac, animated: true, completion: nil)
+        }
     }
     
 
